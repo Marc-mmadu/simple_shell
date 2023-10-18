9d01b1df462c6c9a1309af30daf2aa456b3f2aed
Marc-mmadu <98237139+Marc-mmadu@users.noreply.github.com>
2023-10-19 00:10:05 +0100

    Update memory_functions1.c

--- memory_functions1.c
+++ memory_functions1.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
- *@s: the pointer to the memory area
+ **_memset - fill memory with a constant byte
+ *@s: the pointers to the memory area
  *@b: the byte to fill *s with
  *@n: the amount of bytes to be filled
  *Return: (s) a pointer to the memory area s

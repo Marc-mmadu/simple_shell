983d5e76856da2dfbfcd1d8ff7e2bbe582843139
Marc-mmadu <98237139+Marc-mmadu@users.noreply.github.com>
2023-10-19 00:07:52 +0100

    Update chain.c

--- chain.c
+++ chain.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * is_chain - test if current char in buffer is a chain delimeter
+ * is_chain - test if the current char in the buffer is a chain delimiter
  * @info: the parameter struct
  * @buf: the char buffer
  * @p: address of current position in buf
  *
- * Return: 1 if chain delimeter, 0 otherwise
+ * Return: 1 if chain delimiter, 0 otherwise
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
